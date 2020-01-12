203a6ec31f626f1ec1b8538990b7f8f1f6194be2
Thibaud Laguérie <thibaud.laguerie1@gmail.com>
2020-01-12 21:16:39 +0100

    Add colors to design, going to display list of moves

--- app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailActivity.kt
+++ app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailActivity.kt
@@ -1,22 +1,29 @@
 package com.benjaminledet.pokedex.ui.pokemon.detail
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import com.benjaminledet.pokedex.R
+import com.benjaminledet.pokedex.data.model.Pokemon
+import com.benjaminledet.pokedex.data.model.PokemonDetail
 import com.benjaminledet.pokedex.data.repository.utils.Status
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_pokemon_detail.*
+import org.koin.android.ext.android.get
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PokemonDetailActivity: AppCompatActivity() {
 
     private val viewModel by viewModel<PokemonDetailViewModel>()
 
+    @SuppressLint("ResourceAsColor", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_detail)
@@ -31,22 +38,39 @@ class PokemonDetailActivity: AppCompatActivity() {
         })
 
         viewModel.pokemon.observe(this, Observer { pokemon ->
+
             title = pokemon?.name
             weight.text = getString(R.string.pokemon_weight, pokemon?.detail?.weight.toString())
             height.text = getString(R.string.pokemon_height, pokemon?.detail?.height.toString())
-            var background = "#FFFFFF"
-            if(pokemon?.detail?.types?.get(0).equals("poison")) {
-                 background = "#A040A0"
+
+            // Recherche de la couleur compatible avec le type pour l'afficher en backgroundColor
+            // Couleurs issus du site https://pokemon.fandom.com/wiki/Types en utilisant l'inspecteur d'élément
+            // On considère que le deuxième type, si le pokémon en possède 2, est le type principal
+            val backgroundFirstType = switchToSetBackgroundColor(pokemon, 0)
+            type2.text = getString(R.string.pokemon_type, pokemon?.detail?.types?.get(0).toString().capitalize())
+            cardTypes2.setCardBackgroundColor(this.getColor(backgroundFirstType))
+            cardTypes.isVisible = true
+            if(pokemon?.detail?.types?.size == 2) {
+
+                val backgroundSecondType = switchToSetBackgroundColor(pokemon, 1)
+                content.setBackgroundColor(this.getColor(switchToSetBackgroundColor(pokemon, 3)))
+                cardTypes.setCardBackgroundColor(this.getColor(backgroundSecondType))
+                type.text = getString(R.string.pokemon_type, pokemon?.detail?.types?.get(1).toString().capitalize())
+                cardHeight.setCardBackgroundColor(this.getColor(backgroundSecondType))
+                cardWeight.setCardBackgroundColor(this.getColor(backgroundSecondType))
+            } else {
+                content.setBackgroundColor(this.getColor(switchToSetBackgroundColor(pokemon, 2)))
+                cardHeight.setCardBackgroundColor(this.getColor(backgroundFirstType))
+                cardWeight.setCardBackgroundColor(this.getColor(backgroundFirstType))
+                cardTypes.isVisible = false
             }
-            type.text = getString(R.string.pokemon_type, pokemon?.detail?.types.toString())
-            type.setBackgroundColor(Color.parseColor(background))
             move.text = getString(R.string.pokemon_move, pokemon?.detail?.moves.toString())
 
             Picasso.get().load(pokemon?.iconUrl).into(icon)
         })
 
         viewModel.moves.observe(this, Observer { moves ->
-            Log.d("PokemonDetailActivity", "moves : $moves")
+            Log.d("PokemonDetailActivity", "moves : $moves.")
         })
     }
 
@@ -57,5 +81,68 @@ class PokemonDetailActivity: AppCompatActivity() {
         }
         else -> true
     }
+
+    /**
+     * Permet de choisir la couleur à utiliser selon le type, et selon l'utilisation que l'on souhaite en faire.
+     * @param pokemon
+     * @param index
+     * @return Un entier correspondant à une couleur dans colors.xml
+     */
+   fun switchToSetBackgroundColor(pokemon: Pokemon?, index: Int): Int {
+       // Si l'index est inférieur ou égal à 1, la fonction est utilisé dans le cadre de la couleur de fond du type  correspondant
+       if(index <= 1) {
+           return when(pokemon?.detail?.types?.get(index)) {
+               "normal" -> R.color.normalType
+               "fire" -> R.color.fireType
+               "water" -> R.color.waterType
+               "grass" -> R.color.grassType
+               "electric" -> R.color.electricType
+               "ice" -> R.color.iceType
+               "fighting" -> R.color.fightingType
+               "poison" -> R.color.poisonType
+               "ground" -> R.color.groundType
+               "flying" -> R.color.flyingType
+               "psychic" -> R.color.psychicType
+               "bug" -> R.color.bugType
+               "rock" -> R.color.rockType
+               "ghost" -> R.color.ghostType
+               "dark" -> R.color.darkType
+               "dragon" -> R.color.dragonType
+               "steel" -> R.color.steelType
+               "fairy" -> R.color.fairyType
+               // Des tests effectués ont eu des crash pour certains pokémons en raison d'un problème par rapport à Color.WHITE, décision de le passer en tant que resource dans colors.xml (ex de crash: Caterpie)
+               else -> R.color.noTypeError
+           }
+           // Si l'index est supérieur à 1, la fonction est utilisé dans le cadre de la couleur de fond de l'écran entier
+       } else {
+           var i = 0
+           // Si l'index est égal à 3, cela signifie que le pokémon a 2 types et on choisit la variante du 2ème type dans ce cas là
+           if(index == 3) {
+               i = 1
+           }
+           return when(pokemon?.detail?.types?.get(i)) {
+               "normal" -> R.color.normalTypeVariant
+               "fire" -> R.color.fireTypeVariant
+               "water" -> R.color.waterTypeVariant
+               "grass" -> R.color.grassTypeVariant
+               "electric" -> R.color.electricTypeVariant
+               "ice" -> R.color.iceTypeVariant
+               "fighting" -> R.color.fightingTypeVariant
+               "poison" -> R.color.poisonTypeVariant
+               "ground" -> R.color.groundTypeVariant
+               "flying" -> R.color.flyingTypeVariant
+               "psychic" -> R.color.psychicTypeVariant
+               "bug" -> R.color.bugTypeVariant
+               "rock" -> R.color.rockTypeVariant
+               "ghost" -> R.color.ghostTypeVariant
+               "dark" -> R.color.darkTypeVariant
+               "dragon" -> R.color.dragonTypeVariant
+               "steel" -> R.color.steelTypeVariant
+               "fairy" -> R.color.fairyTypeVariant
+               // Des tests effectués ont eu des crash pour certains pokémons en raison d'un problème par rapport à Color.WHITE, décision de le passer en tant que resource dans colors.xml (ex de crash: Caterpie)
+               else -> R.color.noTypeError
+           }
+       }
+   }
 
 }
--- app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailFragment.kt
+++ app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailFragment.kt
@@ -8,9 +8,24 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.benjaminledet.pokedex.R
+import com.benjaminledet.pokedex.data.model.Pokemon
 import com.benjaminledet.pokedex.data.repository.utils.Status
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_pokemon_detail.*
 import kotlinx.android.synthetic.main.fragment_pokemon_detail.*
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.cardHeight
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.cardTypes
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.cardTypes2
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.cardWeight
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.content
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.height
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.icon
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.move
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.progressBar
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.type
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.type2
+import kotlinx.android.synthetic.main.fragment_pokemon_detail.weight
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PokemonDetailFragment: Fragment() {
@@ -35,7 +50,94 @@ class PokemonDetailFragment: Fragment() {
             weight.text = getString(R.string.pokemon_weight, pokemon?.detail?.weight.toString())
             height.text = getString(R.string.pokemon_height, pokemon?.detail?.height.toString())
 
+            // Recherche de la couleur compatible avec le type pour l'afficher en backgroundColor
+            // Couleurs issus du site https://pokemon.fandom.com/wiki/Types en utilisant l'inspecteur d'élément
+            // On considère que le deuxième type, si le pokémon en possède 2, est le type principal
+            val backgroundFirstType = switchToSetBackgroundColor(pokemon, 0)
+            type2.text = getString(R.string.pokemon_type, pokemon?.detail?.types?.get(0).toString().capitalize())
+
+            cardTypes2.setCardBackgroundColor(requireContext().getColor(backgroundFirstType))
+            cardTypes.isVisible = true
+            if(pokemon?.detail?.types?.size == 2) {
+
+                val backgroundSecondType = switchToSetBackgroundColor(pokemon, 1)
+                content.setBackgroundColor(requireContext().getColor(switchToSetBackgroundColor(pokemon, 3)))
+                cardTypes.setCardBackgroundColor(requireContext().getColor(backgroundSecondType))
+                type.text = getString(R.string.pokemon_type, pokemon?.detail?.types?.get(1).toString().capitalize())
+                cardHeight.setCardBackgroundColor(requireContext().getColor(backgroundSecondType))
+                cardWeight.setCardBackgroundColor(requireContext().getColor(backgroundSecondType))
+            } else {
+                content.setBackgroundColor(requireContext().getColor(switchToSetBackgroundColor(pokemon, 2)))
+                cardHeight.setCardBackgroundColor(requireContext().getColor(backgroundFirstType))
+                cardWeight.setCardBackgroundColor(requireContext().getColor(backgroundFirstType))
+                cardTypes.isVisible = false
+            }
+            move.text = getString(R.string.pokemon_move, pokemon?.detail?.moves.toString())
+
             Picasso.get().load(pokemon?.iconUrl).into(icon)
         })
+    }
+
+    /**
+     * Permet de choisir la couleur à utiliser selon le type, et selon l'utilisation que l'on souhaite en faire.
+     * @param pokemon
+     * @param index
+     * @return Un entier correspondant à une couleur dans colors.xml
+     */
+    fun switchToSetBackgroundColor(pokemon: Pokemon?, index: Int): Int {
+        // Si l'index est inférieur ou égal à 1, la fonction est utilisé dans le cadre de la couleur de fond du type  correspondant
+        if(index <= 1) {
+            return when(pokemon?.detail?.types?.get(index)) {
+                "normal" -> R.color.normalType
+                "fire" -> R.color.fireType
+                "water" -> R.color.waterType
+                "grass" -> R.color.grassType
+                "electric" -> R.color.electricType
+                "ice" -> R.color.iceType
+                "fighting" -> R.color.fightingType
+                "poison" -> R.color.poisonType
+                "ground" -> R.color.groundType
+                "flying" -> R.color.flyingType
+                "psychic" -> R.color.psychicType
+                "bug" -> R.color.bugType
+                "rock" -> R.color.rockType
+                "ghost" -> R.color.ghostType
+                "dark" -> R.color.darkType
+                "dragon" -> R.color.dragonType
+                "steel" -> R.color.steelType
+                "fairy" -> R.color.fairyType
+                // Des tests effectués ont eu des crash pour certains pokémons en raison d'un problème par rapport à Color.WHITE, décision de le passer en tant que resource dans colors.xml (ex de crash: Caterpie)
+                else -> R.color.noTypeError
+            }
+            // Si l'index est supérieur à 1, la fonction est utilisé dans le cadre de la couleur de fond de l'écran entier
+        } else {
+            var i = 0
+            // Si l'index est égal à 3, cela signifie que le pokémon a 2 types et on choisit la variante du 2ème type dans ce cas là
+            if(index == 3) {
+                i = 1
+            }
+            return when(pokemon?.detail?.types?.get(i)) {
+                "normal" -> R.color.normalTypeVariant
+                "fire" -> R.color.fireTypeVariant
+                "water" -> R.color.waterTypeVariant
+                "grass" -> R.color.grassTypeVariant
+                "electric" -> R.color.electricTypeVariant
+                "ice" -> R.color.iceTypeVariant
+                "fighting" -> R.color.fightingTypeVariant
+                "poison" -> R.color.poisonTypeVariant
+                "ground" -> R.color.groundTypeVariant
+                "flying" -> R.color.flyingTypeVariant
+                "psychic" -> R.color.psychicTypeVariant
+                "bug" -> R.color.bugTypeVariant
+                "rock" -> R.color.rockTypeVariant
+                "ghost" -> R.color.ghostTypeVariant
+                "dark" -> R.color.darkTypeVariant
+                "dragon" -> R.color.dragonTypeVariant
+                "steel" -> R.color.steelTypeVariant
+                "fairy" -> R.color.fairyTypeVariant
+                // Des tests effectués ont eu des crash pour certains pokémons en raison d'un problème par rapport à Color.WHITE, décision de le passer en tant que resource dans colors.xml (ex de crash: Caterpie)
+                else -> R.color.noTypeError
+            }
+        }
     }
 }
